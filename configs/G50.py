b29b6ecfd90fb65660223a08dda2668fc37a75d3
JasonLuo701 <nineballin701@gmail.com>
2017-04-28 03:24:55 +0800

    fix
    Signed-off-by: JasonLuo701 <nineballin701@gmail.com>

--- configs/G50.py
+++ configs/G50.py
@@ -562,7 +562,13 @@ HWMON_CONFIG = {
 	},
 	'24-004c' :  {
 		'names' : {
-			'FPGA_amb' : { 'object_path' : 'pcie/FPGA_amb','poll_interval' : 5000,'scale' : 1000,'units' : 'C', 'sensor_type' : '0x01', 'sensornumber' : '0x75',
+			'temp1_input' : { 'object_path' : 'pcie/FPGA_ambtmp','poll_interval' : 5000,'scale' : 1000,'units' : 'C', 'sensor_type' : '0x01', 'sensornumber' : '0x75',
+				'sensor_name':'FPGA Ambient Temp', 'reading_type' : '0x01', 'critical_upper' : 35, 'emergency_enabled' : True },
+		}
+	},
+	'24-004c' :  {
+		'names' : {
+			'temp2_input' : { 'object_path' : 'pcie/FPGA_dietmp','poll_interval' : 5000,'scale' : 1000,'units' : 'C', 'sensor_type' : '0x01', 'sensornumber' : '0x74',
 				'sensor_name':'FPGA Ambient Temp', 'reading_type' : '0x01', 'critical_upper' : 83, 'emergency_enabled' : True },
 		}
 	},
